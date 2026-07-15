84c76d50b1e36b663d07f330c32c62a9f6c6c716
chishiya2309 <lequanghung.work@gmail.com>
2026-07-15 16:59:19 +0700

    Bỏ cấu hình cứng, thay bằng biến môi trường

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import type { LoginFormData, RegisterFormData } from '../utils/auth-schemas';
 
-const API_URL = 'http://localhost:8080/api/auth';
+const API_URL = import.meta.env.VITE_API_URL ? `${import.meta.env.VITE_API_URL}/auth` : 'http://localhost:8080/api/auth';
 
 export const authService = {
   login: async (data: LoginFormData) => {
